507c45020d7a4fc6b183f1e82c8665caee4f953c
Tyson Call <Tyson@Savannahs-MacBook-Air.local>
2019-10-11 21:11:39 -0600

    First

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Daily-Planner
+1. 9-5 time slots (DONE)
+2. Make inputs for middle section "events" ***Add array for button
+3. Add button listener for input to local storage "save button"
+4. Color coordinate past, present, future
---
+++ script.js
@@ -0,0 +1,44 @@
+console.log(moment());
+
+$(document).ready(function() {
+    //current date and time
+    var m = moment();
+    var currentTime = m.format('MMMM Do YYYY, h:mm:ss a');
+    var currentDate = m.format("MMM Do YYYY");
+    console.log(m);
+    console.log(currentTime);
+    $("#currentDay").text("Today's Date: " + currentDate);
+    //timeblocks -----
+    //create render functions
+    //render new event
+    //create click functions
+    //add event function
+    var timeStamps = ["9:00am", "10:00am", "11:00am", "12:00am", "1:00pm", "2:00pm", "3:00pm", "4:00pm", "5:00pm"]
+
+    for(var i = 0; i < timeStamps.length; i++){
+        var makeEventDiv = $("<div class='col-2'></div>");
+        var makeEventButton = $("<button>")
+        var timeDiv = $("<div class='col-2'><p>" + timeStamps[i] + "</p></div>");
+        var eventDiv = $("<div class='col-8'><input type='text' id='eventInput' data-value='" + timeStamps[i] + "'></div>");
+        makeEventButton.addClass("event-button");
+        makeEventButton.text("Save");
+        
+    
+        
+        $(".row").append(timeDiv);
+        $(".row").append(eventDiv);
+        $(".row").append(makeEventButton);
+        console.log("making buttons");
+
+        $(".event-button").on("click", function(event) {
+            var buttonTime = $(".event-button").attr("value");
+            event.preventDefault();
+            var eventStorage = $("#eventInput").val().trim();
+            localStorage.setItem("", eventStorage);
+        });
+
+    
+    }
+
+    
+ });
